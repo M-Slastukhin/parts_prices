864a364f23d5f71777d080dc52dcac2c47787f0e
M-Slastukhin <100306561+M-Slastukhin@users.noreply.github.com>
2022-03-06 17:59:20 +0400

    added dict exist_result

--- pars.py
+++ pars.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
 def exist_parser(part_number):
+    exist_result = {}
     options = Options()
     options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
@@ -13,12 +14,14 @@ def exist_parser(part_number):
     driver.get(exist_url)
     exist_price_body = driver.find_element(By.CLASS_NAME, value='table-body')
     exist_allOffers = exist_price_body.find_element(By.CLASS_NAME, value='allOffers')
-
-    #print(exist_allOffers.text)
+    exist_delivery = exist_allOffers.find_element(By.CLASS_NAME, value='statis')
+    exist_price = exist_allOffers.find_element(By.CLASS_NAME, value='price')
+    exist_result[exist_price.text] = exist_delivery.text
+    print(exist_result)
     return (exist_allOffers)
 
 
     #driver.close()
 
 part_number = '164005420R'
-print (exist_parser(part_number).text)
+print(exist_parser(part_number).text)
