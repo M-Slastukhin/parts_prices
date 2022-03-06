db7b5286f8e8c1bf8e23c32ec0d96f11d3baaeea
M-Slastukhin <100306561+M-Slastukhin@users.noreply.github.com>
2022-03-06 17:36:04 +0400

    Commit

---
+++ pars.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+#from selenium import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+def exist_parser(part_number):
+    #options = Options()
+    #options.add_argument('--headless') options=options
+    driver = webdriver.Chrome()
+    exist_url = "https://www.exist.ru/Price/?pcode=164005420R"
+    driver.get(exist_url)
+    exist_price_body = driver.find_element(By.CLASS_NAME, value='table-body')
+    exist_allOffers = exist_price_body.find_element(By.CLASS_NAME, value='allOffers')
+    return (exist_allOffers)
+
+
+    #driver.close()
+
+part_number = '164005420R'
+print (exist_parser(part_number).text)
